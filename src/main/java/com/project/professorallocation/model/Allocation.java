3da847739f734ee3246df20b1751bed50d0e98ec
Lorena Grinberg <lorena.adecon@gmail.com>
2022-04-14 15:53:29 -0300

    Add model layer

---
+++ src/main/java/com/project/professorallocation/model/Allocation.java
@@ -0,0 +1,127 @@
+package com.project.professorallocation.model;
+
+import java.time.DayOfWeek;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "allocation")
+public class Allocation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, name = "day")
+	private DayOfWeek dayofweek;
+	
+	@Temporal(TemporalType.TIME)
+	@Column(nullable = false)
+	private Date startHour;
+		
+	@Column(nullable = false)
+	private Date endHour;
+
+	@Column(name = "professor_id", nullable = false)
+	private Long professorId;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "professor_id", nullable = false, insertable = false, updatable = false)
+	private Professor professor;
+	
+	@Column(name = "course_id", nullable = false)
+	private Long courseId;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "course_id", nullable = false, insertable = false, updatable = false)
+	private Course course;
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public Allocation() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDayofweek() {
+		return dayofweek;
+	}
+
+	public void setDayofweek(DayOfWeek dayofweek) {
+		this.dayofweek = dayofweek;
+	}
+
+	public Date getStartHour() {
+		return startHour;
+	}
+
+	public void setStartHour(Date startHour) {
+		this.startHour = startHour;
+	}
+
+	public Date getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(Date endHour) {
+		this.endHour = endHour;
+	}
+
+	public Long getProfessorId() {
+		return professorId;
+	}
+
+	public void setProfessorId(Long professorId) {
+		this.professorId = professorId;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	@Override
+	public String toString() {
+		return "Allocation [id=" + id + ", dayofweek=" + dayofweek + ", startHour=" + startHour + ", endHour=" + endHour
+				+ ", professorId=" + professorId + ", courseId=" + courseId + "]";
+	}
+
+
+	
+}
