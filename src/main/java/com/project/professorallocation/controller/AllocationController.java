c25c0f64e3aace3d50e86dee0934b7915cec8840
Lorena Grinberg <lorena.adecon@gmail.com>
2022-05-08 18:36:22 -0300

    Add annotation api operation and response in allocation controller layer

--- src/main/java/com/project/professorallocation/controller/AllocationController.java
+++ src/main/java/com/project/professorallocation/controller/AllocationController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.professorallocation.model.Allocation;
 import com.project.professorallocation.service.AllocationService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(path = "/allocations")
 public class AllocationController {
@@ -28,7 +32,7 @@ public class AllocationController {
 		super();
 		this.service = service;
 	}
-
+	@ApiOperation(value = "Find all allocations")
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<List<Allocation>> findAll() {
@@ -36,7 +40,11 @@ public class AllocationController {
 
 		return new ResponseEntity<List<Allocation>>(allItems, HttpStatus.OK);
 	}
-
+	@ApiOperation(value = "Find an allocation by id")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "OK"),
+		@ApiResponse(code = 404, message = "Allocation not found")
+	})
 	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<Allocation> findById(@PathVariable(name = "id") Long id) {
@@ -104,3 +112,4 @@ public class AllocationController {
 
 //curl -v --request POST --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"dayofweek\": \"SUNDAY\", \"startHour\": \"19:35-0300\", \"endHour\": \"21:35-0300\", \"professorId\": \"7\", \"courseId\": \"1\"}" "http://localhost:8080/allocations"
 //curl -v --request PUT --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"dayofweek\": \"WEDNESDAY\",\"startHour\": \"19:18-0300\", \"endHour\": \"21:18-0300\",\"professorId\": \"5\", \"courseId\": \"5\"}" "http://localhost:8080/allocations/9"
+//curl -v --request PUT --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"dayofweek\": \"WEDNESDAY\",\"startHour\": \"19:18-0300\", \"endHour\": \"21:18-0300\",\"professorId\": \"1\", \"courseId\": \"3\"}" "http://localhost:8080/allocations/12"
